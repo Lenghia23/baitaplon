6f0f56fe08ecaa92f754dd2d5e0c7ea2491b9980
Lenghia23 <letrungnghia230803@gmail.com>
2023-09-25 20:14:15 +0700

    Baitaplon

---
+++ BusinessLogicLayer/HoaDonBusiness.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLogicLayer;
+using DataAccessLayer;
+using DataModel;
+
+namespace BusinessLogicLayer
+{
+    public class HoaDonBusiness:IHoaDonBusiness
+    {
+        private IHoaDonRepository _res;
+        public HoaDonBusiness(IHoaDonRepository res)
+        {
+            _res = res;
+        }
+        public bool Create(HoaDonModel model)
+        {
+            return _res.Create(model);
+        }
+        public bool Update(HoaDonModel model)
+        {
+            return _res.Update(model);
+        }
+    }
+}
